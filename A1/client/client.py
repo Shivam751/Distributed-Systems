aa62c652a5497109a8419726c39cf5f62fb4875e
Shivam751 <shivsrj2580@gmail.com>
2024-01-24 21:52:20 +0530

    Client ultility functions

---
+++ A1/client/client.py
@@ -0,0 +1,28 @@
+import requests
+import aiohttp
+import asyncio
+import re
+import matplotlib.pyplot as plt
+
+def extract_serv_id(input_string):
+    match = re.search(r"Hello from Server: (\S+)", input_string)
+    if match:
+        return match.group(1)  # This is the server ID
+    else:
+        return None
+def add(n = 1, hostnames = ["s1"]):
+    r = requests.post('http://localhost:5000/add', json={"n": n, "hostnames": hostnames})
+    print(r.json())
+
+def rm(n = 1, hostnames = ["s1"]):
+    r = requests.delete('http://localhost:5000/rm', json={"n": n, "hostnames": hostnames})
+    print(r.json())
+
+
+async def main():
+    url = 'http://localhost:5000/home'
+    responses = None
+    dic = {}
+
+if __name__ == '__main__':
+    asyncio.run(main())
