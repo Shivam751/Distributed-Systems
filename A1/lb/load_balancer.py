dbf28e8c162697039e3457eeea0a4661bf962ab3
Aniket Kumar <ani30r.ak@gmail.com>
2024-01-24 21:42:56 +0530

    Added Monitor

--- A1/lb/load_balancer.py
+++ A1/lb/load_balancer.py
@@ -25,3 +25,27 @@ async def check_heartbeat(server_name = None):
                     return False
     except Exception as e:
         return False
+    
+async def periodic_server_monitor(interval = 1):
+    while True:
+        dead_servers = []
+        tasks = [check_heartbeat(server_name) for server_name in server_hostname_to_id.keys()]
+        results = await asyncio.gather(*tasks)
+        results = zip(server_hostname_to_id.keys(), results)
+        for server_name, result in results:
+            if result == False:
+                server_id = server_hostname_to_id[server_name]
+                ch.remove_server(server_id)
+                dead_servers.append(server_name)
+        
+        for server_name in dead_servers:
+            server_id = server_hostname_to_id[server_name]
+
+            try:
+                res = client.containers.run(image=image, name=server_name, network=network, detach=True, environment={'SERV_ID': server_id})
+            except Exception as e:
+                print(e)
+
+            ch.add_server(server_id)
+        
+        await asyncio.sleep(interval)
